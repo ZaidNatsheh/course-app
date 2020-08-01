3bf3f9a0c325e3233474a8ceb44c90fb955b837e
ZaidNatsheh <66726824+ZaidNatsheh@users.noreply.github.com>
2020-08-02 01:02:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { IonApp, IonRouterOutlet } from "@ionic/react";
-import { Route, Redirect } from "react-router-dom";
+import { IonApp, IonRouterOutlet,IonTabs,
+  IonTabButton,
+  IonTabBar,
+  IonIcon,
+  IonLabel } from "@ionic/react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import { IonReactRouter } from "@ionic/react-router";
 import Filter from "./pages/Filter";
 import CourseTabs from "./pages/CourseTabs";
@@ -21,7 +25,10 @@ import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-
+import { list, trophyOutline } from "ionicons/icons";
+import Courses from "./pages/Courses";
+import AllGoals from "./pages/AllGoals";
+import CourseGoals from "./pages/CourseGoals";
 /* Theme variables */
 import "./theme/variables.css";
 import "./theme/theme.css";
@@ -32,17 +39,57 @@ const App: React.FC = () => (
     <IonReactRouter>
       <SideDrawer />
       <CoursesContextProvider>
-        <IonRouterOutlet id="main">
+      <IonTabs>
+      <IonRouterOutlet id="main">
+        <Switch>
+        <Redirect from="/" to="/courses/all-goals"  exact />
+        {/* <Redirect from="/courses" to="/courses/all-goals"  exact /> */}
+        <Route path="/courses/lists" exact>
+          <Courses />
+        </Route>
+       
+        <Route path="/courses/all-goals" exact>
+          <AllGoals />
+        </Route>
+        <Route path="/courses/:courseId">
+          <CourseGoals />
+        </Route>
+        <Route path="/courses" exact>
+            <CourseTabs />
+            {console.log("*courses from app")}
+          </Route>
+        <Route path="/filter" exact>
+            <Filter />
+            {console.log("*f from app")}
+          </Route>
+       
+        </Switch>
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="all-goals" href="/courses/all-goals">
+          <IonIcon icon={list} />
+          <IonLabel>All Goals</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="courses" href="/courses/lists">
+          <IonIcon icon={trophyOutline} />
+          <IonLabel>Courses</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+        {/* <IonRouterOutlet id="main">
+          {/* <Switch > 
           <Route path="/filter" exact>
             <Filter />
+            {console.log("*f from app")}
           </Route>
           <Route path="/courses" exact>
             <CourseTabs />
+            {console.log("*courses from app")}
           </Route>
           <Redirect path="/" to="/courses" exact />
-        </IonRouterOutlet>{" "}
+          {/* </Switch> 
+        </IonRouterOutlet>{" "} */}
       </CoursesContextProvider>
-      ,
     </IonReactRouter>
   </IonApp>
 );
